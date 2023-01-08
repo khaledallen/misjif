f066ee19e7dde8bdaa9570ae7b824dbd191d9bcc
Khaled Allen <khaled.allen@gmail.com>
2023-01-07 21:03:12 -0800

    get the list of post names, still need to actually find a way to generate the index template

--- lib/misjif.dart
+++ lib/misjif.dart
@@ -3,17 +3,18 @@ import 'dart:io';
 import 'package:misjif/templater.dart';
 import 'post.dart';
 
-Future<void> printPostToConsole(String path) async {
-  Post post = Post(path);
-  String postHtml = buildPost(post);
-  print(postHtml);
+String buildPostUrl(Post post) {
+  String newName = post.path.split('/')[1].split('.')[0];
+  return '<a href="posts/$newName.html">$newName</a>';
 }
 
-Future<void> generatePostFiles(String path) async {
-  Post post = Post(path);
+Future<void> generatePostFile(Post post, {bool debug = false}) async {
   String postHtml = buildPost(post);
+  if(debug) {
+    print(postHtml);
+  }
 
-  String newName = path.split('/')[1].split('.')[0];
+  String newName = post.path.split('/')[1].split('.')[0];
   try {
     var file = await File('public/posts/$newName.html').create(recursive: true);
     file.writeAsStringSync(postHtml);
@@ -25,10 +26,16 @@ Future<void> generatePostFiles(String path) async {
 void processPosts(String dir) {
   var postDirectory = Directory(dir);
   var posts = postDirectory.listSync();
+  List<Post> postList = [];
+  List<String> postLinks = [];
   for(var file in posts) {
     if(file.path.contains('.md')) {
-      printPostToConsole(file.path);
-      generatePostFiles(file.path);
+      Post post = Post(file.path);
+      postList.add(post);
+      generatePostFile(post, debug: true);
+      postLinks.add(buildPostUrl(post));
     }
   }
+  print(postLinks);
+
 }
