36e0fb968008c507efabdc2998deb7655432ed22
Khaled Allen <khaled.allen@gmail.com>
2023-02-10 12:21:14 -0800

    in the process of reworking the parser to generate richer statement types for the parsed array

--- lib/renderer.dart
+++ lib/renderer.dart
@@ -8,12 +8,13 @@ class Renderer {
 
   render() {
     for (int i = 0; i < parsed.length; i++) {
+      print('1');
       var token = parsed[i];
+      print(token);
       if(token.contains('{{')) {
         parsed[i] = insertData(token);
       }
     }
-    print(parsed);
     return parsed.join();
   }
 
@@ -21,7 +22,6 @@ class Renderer {
     var prop = token.replaceAll(RegExp('{{|}}'), '')
       .trim().split('.')[1];
     try {
-      print(postData[prop]);
       return postData[prop];
     } catch (e) {
       print('Property $prop not defined on this post type: $e');
